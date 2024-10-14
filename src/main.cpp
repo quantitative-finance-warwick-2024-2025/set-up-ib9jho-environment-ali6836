00c21183d26fb4f893bd79de96ed571aca0e163b
ali6836 <tigerali1@yahoo.co.uk>
2024-10-14 14:15:41 +0100

    Submission

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-int main ()
-{
-    // we want to print "My C Compiler is working!" instead
-    std::cout << "Change me before you submit the assignment!";
+int main() {
+  // we want to print "My C Compiler is working!" instead
+  std::cout << "My C Compiler is working!";
 }
